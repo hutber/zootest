504f3316d21345727411c8b24b2c1f2f81f76ea6
Jamie Hutber <jamie@hutber.com>
2017-04-05 12:57:00 +0100

    chore: Add the start of tests

---
+++ test/helper/registerBabel.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/no-commonjs, import/unambiguous */
+require('babel-register')();
---
+++ test/unit/splash/index.spec.js
@@ -0,0 +1,8 @@
+/* eslint-disable object-property-newline */
+import {expect} from 'chai';
+
+describe('Splash', () => {
+  it('must be defined', () => {
+    expect(Splash).to.be.defined;
+  })
+});
