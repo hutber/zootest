447866f69b3080b62934c1c7a7f6774f42d650bf
Jamie Hutber <jamie@hutber.com>
2017-04-06 11:21:32 +0100

    feat: added loader

---
+++ src/templates/errors/index.js
@@ -0,0 +1,8 @@
+//- Created by hutber on 06/04/17.  */
+import searchMessages from './search';
+
+ const errors = {
+	searchMessages: searchMessages
+};
+
+export default errors;
---
+++ src/templates/errors/search.js
@@ -0,0 +1,9 @@
+//- Created by hutber on 06/04/17.  */
+const handlebars = require('handlebars');
+
+const searchMessages = {
+	emptySearch: handlebars.compile(`<p>Please enter a search term</p>`),
+	noResults: handlebars.compile(`<p>No Results Found</p>`)
+}
+
+export default searchMessages;
